5fb9f2519d8ef4ebb30e9360abb65479576c7a67
ihsanannashir <ihsanannashir@gmail.com>
2023-04-11 16:07:42 +0700

    set tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './index.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'very-dark-blue': '#090b1a',
+        'desaturated-blue': '#1b1938',
+        'soft-violet': '#aa5cdb',
+        'white-second': 'hsla(0, 0%, 100%, 0.75)',
+        'white-third': 'hsla(0, 0%, 100%, 0.6)',
+      },
+    },
   },
   plugins: [],
 }
-
